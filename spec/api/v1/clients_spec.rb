6d423913de3eb743b58cdb2395231cbab6a537ff
Sergei Anisimov <pearce89@gmail.com>
2015-03-08 01:04:33 +0300

    one more api test

--- spec/api/v1/clients_spec.rb
+++ spec/api/v1/clients_spec.rb
@@ -19,4 +19,12 @@ describe "Clients API" do
     stored_client = Client.find_by_invoice_id(999)
     expect(stored_client.as_json.except('id', 'created_at', 'updated_at')).to eq(new_client_parameters)
   end
+
+  it 'gets an errors with a wrong new Client parameters' do
+    new_client_parameters = { 'invoice_id' => 999, 'fiscal_id' => 5555, 'name' => 'Sergei', 'email' => 'sergeianisimov.co' }
+    post "/store-bahamas-client/#{new_client_parameters['invoice_id']}", new_client_parameters.except('invoice_id'), { 'HTTP_ACCEPT' => 'application/json; version=1' }
+
+    expect(response).to be_unprocessable
+    expect(json['errors']).to eq(['Email is invalid'])
+  end
 end
